215ef7b20386240544a2612cae32f502162524f8
Sebastian Piwnicki <seba.piwnicki@gmail.com>
2017-11-13 19:39:21 +0000

    Work you piece of shit

--- aya.py
+++ aya.py
@@ -22,18 +22,7 @@ async def on_event(self):
 	
 @client.event
 async def on_message(message):
-	if message.content.startswith('!test'):
-		counter = 0
-		tmp = await client.send_message(message.channel, 'Calculating messages...')
-		async for log in client.logs_from(message.channel, limit=100):
-			if log.author == message.author:
-				counter += 1
-
-		await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-	elif message.content.startswith('!sleep'):
-		await asyncio.sleep(5)
-		await client.send_message(message.channel, 'Done sleeping')
-	elif message.content.startswith('test'):
+	if message.content.startswith('Initalize Aya.exe'):
 		while True:
 			channel = client.get_channel('360042247770734593')
 			wiki = "http://www.mynintendo.pl/"
