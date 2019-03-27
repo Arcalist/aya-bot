b27ad44d5d363cec2f27de1a051b767706c3f913
Hokan <seba.piwnicki@gmail.com>
2019-03-27 16:01:41 +0100

    Aya wita sie z szerszym swiatem

--- aya.py
+++ aya.py
@@ -1,4 +1,4 @@
-﻿from urllib.request import urlopen
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from discord.ext import commands
 import discord
@@ -15,60 +15,83 @@ cur = conn.cursor()
 cur.execute("SELECT * FROM token")
 token = cur.fetchone()
 
-
 pages = {"mynintendo": "https://www.mynintendo.pl/",
- "lowcyps4": "https://lowcygier.pl/platforma/ps4/",
- "lowcyswitch": "https://lowcygier.pl/platforma/nintendo-switch/",
- "lowcypc":"https://lowcygier.pl/platforma/pc/",
- "switch":"https://www.mynintendo.pl/category/nintendo-switch/",
- "3ds":"https://www.mynintendo.pl/category/wiadomosci/"}
+         "lowcyps4": "https://lowcygier.pl/platforma/ps4/",
+         "lowcyswitch": "https://lowcygier.pl/platforma/nintendo-switch/",
+         "lowcypc": "https://lowcygier.pl/platforma/pc/",
+         "switch": "https://www.mynintendo.pl/category/nintendo-switch/",
+         "3ds": "https://www.mynintendo.pl/category/wiadomosci/"}
+
+client = commands.Bot(command_prefix=prefix, description=desc)
 
 
-client = commands.Bot(command_prefix = prefix, description = desc)
 @client.event
 async def on_ready():
-	print('Logged in as')
-	print(client.user.id)
-	print('------')
-	
-	admin_channel = client.get_channel('255758512632627200')
-	await client.send_message(admin_channel, 'Initalize Aya.exe')
-	channel = client.get_channel('360042247770734593')
-	lowcy_pc, lowcy_switch, lowcy_ps4 = client.get_channel('373841364053655573'), client.get_channel('477817276368683021'), client.get_channel('477817309067739157')
-	while True:
-			
-		for category, page in pages.items():
-			url = urlopen(page)
-			soup = BeautifulSoup(url)
-			link = soup.h2.a.get("href")
-			if link == "https://lowcygier.pl/polecane/":
-						link = soup.h2.find("a", {"class": "post-title-text"}).get("href")
-			if "lowcy" in category:
-				cur.execute("SELECT * FROM news where link = %s and platform = %s", (link,category))   
-			else:
-				cur.execute("SELECT * FROM news where link = %s", (link,)) 
-			#print(cur.fetchone())
-			if cur.fetchone() is None:
-				if "lowcy" in category:
-					if "ps4" in category:
-						await client.send_message(lowcy_ps4, link)
-					elif "pc" in category:
-						await client.send_message(lowcy_pc, link)
-					else:
-						await client.send_message(lowcy_switch, link)
-					
-				else:
-					await client.send_message(channel, link)
-				cur.execute("UPDATE news SET link = %s WHERE platform = %s",(link, category))
-				conn.commit()  
-				
-			print(category,link)
-		cur.execute("SELECT * FROM news")
-		print(cur.fetchall())
-		
-			
-		await asyncio.sleep(60)
+    print('Logged in as')
+    print(client.user.id)
+    print('------')
+    await client.change_presence(game=discord.Game(name="Ayaya"))
 
 
+async def check_pages():
+    await client.wait_until_ready()
+    admin_channel = client.get_channel('255758512632627200')
+    await client.send_message(admin_channel, 'Initalize Aya.exe')
+    while not client.is_closed:
+
+        for category, page in pages.items():
+            url = urlopen(page)
+            soup = BeautifulSoup(url)
+            link = soup.h2.a.get("href")
+            if link == "https://lowcygier.pl/polecane/":
+                link = soup.h2.find("a", {"class": "post-title-text"}).get("href")
+            if "lowcy" in category:
+                cur.execute("SELECT * FROM news where link = %s and platform = %s", (link, category))
+            else:
+                cur.execute("SELECT * FROM news where link = %s", (link,))
+            # print(cur.fetchone())
+            if cur.fetchone() is None:
+                if "lowcy" in category:
+                    if "ps4" in category:
+                        cur.execute("SELECT channel FROM channels where category = %s", ('lowcyps4',))
+                        for channel in cur.fetchall():
+                            await client.send_message(client.get_channel(''.join(channel)), link)
+                    elif "pc" in category:
+                        cur.execute("SELECT channel FROM channels where category = %s", ('lowcypc',))
+                        for channel in cur.fetchall():
+                            await client.send_message(client.get_channel(''.join(channel)), link)
+                    else:
+                        cur.execute("SELECT channel FROM channels where category = %s", ('lowcyswitch',))
+                        for channel in cur.fetchall():
+                            await client.send_message(client.get_channel(''.join(channel)), link)
+
+                else:
+                    cur.execute("SELECT channel FROM channels where category = %s", ('mynintendo', ))
+                    for channel in cur.fetchall():
+                        await client.send_message(client.get_channel(''.join(channel)), link)
+
+                cur.execute("UPDATE news SET link = %s WHERE platform = %s", (link, category))
+                conn.commit()
+
+            print(category, link)
+        cur.execute("SELECT * FROM news")
+        print(cur.fetchall())
+
+        await asyncio.sleep(60)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('aya track'):
+        msg = message.content.lower().split()
+        if len(msg) == 3 and msg[2] in ['lowcypc, lowcyswitch, lowcyps4, mynintendo']:
+            await client.send_message(message.channel, msg[2])
+            if cur.execute('INSERT INTO channels VALUES(%s, %s)', (message.channel, msg[2])):
+                await client.send_message(message.channel, "Pomyślnie dodano do śledzenia")
+            else:
+                await client.send_message(message.channel, "Ayaya! Coś poszło nie tak")
+        else:
+            await client.send_message(message.channel, "Niestety nie mogę śledzić tej strony")
+
+client.loop.create_task(check_pages())
 client.run(''.join(token))
-
