3fb4b6c3aa782b21a7fb5ec35fed0f27987e1c5a
Hokan <seba.piwnicki@gmail.com>
2019-03-27 16:05:35 +0100

    jeszcze jeden szybki fix

--- aya.py
+++ aya.py
@@ -85,8 +85,7 @@ async def on_message(message):
     if message.content.startswith('aya track'):
         msg = message.content.lower().split()
         if len(msg) == 3 and msg[2] in ['lowcypc', 'lowcyswitch', 'lowcyps4', 'mynintendo']:
-            await client.send_message(message.channel, msg[2])
-            if cur.execute('INSERT INTO channels VALUES(%s, %s)', (message.channel, msg[2])):
+            if cur.execute('INSERT INTO channels VALUES(%s, %s)', (message.channel.id, msg[2])):
                 await client.send_message(message.channel, "Pomyślnie dodano do śledzenia")
             else:
                 await client.send_message(message.channel, "Ayaya! Coś poszło nie tak")
