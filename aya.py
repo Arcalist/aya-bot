ec33709480c4f39edf82935783dbdc7705e0b732
Hokan <seba.piwnicki@gmail.com>
2019-04-27 18:19:07 +0200

    typo fix

--- aya.py
+++ aya.py
@@ -29,7 +29,7 @@ async def on_ready():
     print('Logged in as')
     print(client.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name="'Aya help' for help"))
+    await client.change_presence(game=discord.Game(name="'aya help' for help"))
 
 
 async def check_pages():
