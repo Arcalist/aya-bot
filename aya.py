f53ba87567ca11ccdc132d18189ee4778840893a
Hokan <seba.piwnicki@gmail.com>
2019-04-27 18:11:45 +0200

    zmiana z parsowania htmla na uzywanie rss + help

--- aya.py
+++ aya.py
@@ -1,5 +1,4 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+import feedparser
 from discord.ext import commands
 import discord
 import asyncio
@@ -15,12 +14,12 @@ cur = conn.cursor()
 cur.execute("SELECT * FROM token")
 token = cur.fetchone()
 
-pages = {"mynintendo": "https://www.mynintendo.pl/",
-         "lowcyps4": "https://lowcygier.pl/platforma/ps4/",
-         "lowcyswitch": "https://lowcygier.pl/platforma/nintendo-switch/",
-         "lowcypc": "https://lowcygier.pl/platforma/pc/",
-         "switch": "https://www.mynintendo.pl/category/nintendo-switch/",
-         "3ds": "https://www.mynintendo.pl/category/wiadomosci/"}
+pages = {
+         "mynintendo": "https://www.mynintendo.pl/feed/",
+         "lowcyps4": "https://lowcygier.pl/platforma/ps4/feed/",
+         "lowcyswitch": "https://lowcygier.pl/platforma/nintendo-switch/feed/",
+         "lowcypc": "https://lowcygier.pl/platforma/pc/feed/"
+        }
 
 client = commands.Bot(command_prefix=prefix, description=desc)
 
@@ -30,51 +29,22 @@ async def on_ready():
     print('Logged in as')
     print(client.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name="Ayaya"))
+    await client.change_presence(game=discord.Game(name="'Aya help' for help"))
 
 
 async def check_pages():
     await client.wait_until_ready()
     while not client.is_closed:
-
         for category, page in pages.items():
-            url = urlopen(page)
-            soup = BeautifulSoup(url)
-            link = soup.h2.a.get("href")
-            if link == "https://lowcygier.pl/polecane/":
-                link = soup.h2.find("a", {"class": "post-title-text"}).get("href")
-            if "lowcy" in category:
-                cur.execute("SELECT * FROM news where link = %s and platform = %s", (link, category))
-            else:
-                cur.execute("SELECT * FROM news where link = %s", (link,))
+            news = feedparser.parse(page).entries[0]
+            cur.execute("SELECT * FROM news where link = %s and platform = %s", (news.link, category))
             # print(cur.fetchone())
             if cur.fetchone() is None:
-                if "lowcy" in category:
-                    if "ps4" in category:
-                        cur.execute("SELECT channel FROM channels where category = %s", ('lowcyps4',))
-                        for channel in cur.fetchall():
-                            await client.send_message(client.get_channel(''.join(channel)), link)
-                    elif "pc" in category:
-                        cur.execute("SELECT channel FROM channels where category = %s", ('lowcypc',))
-                        for channel in cur.fetchall():
-                            await client.send_message(client.get_channel(''.join(channel)), link)
-                    else:
-                        cur.execute("SELECT channel FROM channels where category = %s", ('lowcyswitch',))
-                        for channel in cur.fetchall():
-                            await client.send_message(client.get_channel(''.join(channel)), link)
-
-                else:
-                    cur.execute("SELECT channel FROM channels where category = %s", ('mynintendo', ))
-                    for channel in cur.fetchall():
-                        await client.send_message(client.get_channel(''.join(channel)), link)
-
+                cur.execute("SELECT channel FROM channels where category = %s", (category, ))
+                for channel in cur.fetchall():
+                    await client.send_message(client.get_channel(''.join(channel)), link)
                 cur.execute("UPDATE news SET link = %s WHERE platform = %s", (link, category))
                 conn.commit()
-
-            print(category, link)
-        cur.execute("SELECT * FROM news")
-        print(cur.fetchall())
-
         await asyncio.sleep(60)
 
 
@@ -95,6 +65,13 @@ async def on_message(message):
         cur.execute('DELETE FROM channels WHERE channel = (%s)', (str(message.channel.id),))
         conn.commit()
         await client.send_message(message.channel, "Pomyślnie usunięto kanał z powiadomień")
+    if message.content == 'aya help':
+        embed = discord.Embed(title="Szybka pomoc do Ayi", colour=discord.Colour(0x1))
+        embed.add_field(name="aya help", value="Wyświetla tą komendę")
+        embed.add_field(name="aya track 'platforma'", value="Dodaje obecny kanał do wysyłania wiadomości dotyczącej danej platformy\nMożliwe platformy:\nlowcyps4 - lowcygier.pl kategoria z promocjami dotyczące ps4\nlowcyswitch - lowcygier.pl kategoria z promocjami dotycące switcha\nlowcypc - lowcygier.pl kategoria z promocjami dotyczące\nmynintendo - newsy z mynintendo.pl")
+        embed.add_field(name="aya unsub", value="Usuwa wszystkie śledzenia z tego kanału")
+
+        await client.send_message(message.channel, embed=embed)
 
 client.loop.create_task(check_pages())
 client.run(''.join(token))
