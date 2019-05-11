a284da5176cb3c9ce42f6481d012f33dfd5cb164
Hokan <seba.piwnicki@gmail.com>
2019-05-11 16:18:26 +0200

    dodanie obslugi gamehaga (wersja tymczasowa)

--- aya.py
+++ aya.py
@@ -58,7 +58,7 @@ async def check_pages():
 
         if cur.fetchone() is None:
             cur.execute("Select * FROM gamehag ORDER BY timestamp DESC limit 1")
-            most_recent = cur.fetchall()
+            most_recent = cur.fetchone()
 
             f = "%Y-%m-%d %H:%M:%S"
             t_stamp = datetime.strptime(article['created_at'], f)
@@ -97,30 +97,6 @@ async def on_message(message):
 
         await client.send_message(message.channel, embed=embed)
 
-def check_gamehag(cur, conn):
-    response = requests.get('https://gamehag.com/api/v2/news?token=zMoIilzTXnAXXXX')
-    response.encoding = 'utf-8'
-    data = response.json()
-    article = data['collection'][0]
-
-    cur.execute("SELECT * FROM gamehag WHERE timestamp = %s", (article['created_at'], ))
-
-    if cur.fetchone is not None:
-        return False
-
-    cur.execute("Select * FROM gamehag ORDER BY timestamp DESC limit 1")
-    most_recent = cur.fetchone()
-
-    f = "%Y-%m-%d %H:%M:%S"
-    t_stamp = time.strptime(article['created_at'], f)
-    most_recent_stamp = time.strptime(''.join(most_recent), f)
-
-    if t_stamp > most_recent_stamp:
-        cur.execute("INSERT INTO gamehag VALUES(%s)", article['created_at'])
-        conn.commit()
-        return ('https://gamehag.com/pl/artykuly/'+s['url'])
-    else:
-        return False
 
 client.loop.create_task(check_pages())
 client.run(''.join(token))
