c6e7e3d4a17f9a10d1607d38b2ad118c3f044d2c
Hokan <seba.piwnicki@gmail.com>
2019-05-11 15:36:26 +0200

    dodanie obslugi gamehaga (wersja tymczasowa)

--- aya.py
+++ aya.py
@@ -1,4 +1,3 @@
-from gamehag import *
 import feedparser
 from discord.ext import commands
 import discord
@@ -78,5 +77,32 @@ async def on_message(message):
 
         await client.send_message(message.channel, embed=embed)
 
+def check_gamehag(cur, conn):
+    response = requests.get('https://gamehag.com/api/v2/news?token=zMoIilzTXnAXXXX')
+    response.encoding = 'utf-8'
+    data = response.json()
+    article = data['collection'][0]
+
+    cur.execute("SELECT * FROM gamehag WHERE timestamp = %s", (article['created_at'], ))
+
+    if cur.fetchone is not None:
+        return False
+
+    cur.execute("Select * FROM gamehag ORDER BY timestamp DESC limit 1")
+    most_recent = cur.fetchone()
+
+    f = "%Y-%m-%d %H:%M:%S"
+    t_stamp = time.strptime(article['created_at'], f)
+    most_recent_stamp = time.strptime(''.join(most_recent), f)
+
+    if t_stamp > most_recent_stamp:
+        cur.execute("INSERT INTO gamehag VALUES(%s)", article['created_at'])
+        conn.commit()
+        return ('https://gamehag.com/pl/artykuly/'+s['url'])
+    else:
+        return False
+
 client.loop.create_task(check_pages())
 client.run(''.join(token))
+
+
