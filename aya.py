db5bf304b02a488d32d454320b8fab0c6693d36b
Sebastian Piwnicki <seba.piwnicki@gmail.com>
2020-07-25 12:34:12 +0200

    Update aya.py

--- aya.py
+++ aya.py
@@ -40,22 +40,20 @@ async def check_pages():
     print('check pre await')
     await client.wait_until_ready()
     print('check post await')
-    while not client.is_closed:
-        for category, page in pages.items():
-            #try:
-            news = feedparser.parse(page).entries[0]
-            cur.execute("SELECT * FROM news where link = %s and platform = %s", (news.link, category))
-            print(cur.fetchone())
-            if cur.fetchone() is None:
-                cur.execute("SELECT channel FROM channels where category = %s", (category, ))
-                for channel in cur.fetchall():
-                    await client.send_message(client.get_channel(''.join(channel)), news.link)
-                cur.execute("UPDATE news SET link = %s WHERE platform = %s", (news.link, category))
-                conn.commit()
-            #except BaseException:
-                #pass
+    for category, page in pages.items():
+        #try:
+        news = feedparser.parse(page).entries[0]
+        cur.execute("SELECT * FROM news where link = %s and platform = %s", (news.link, category))
+        print(cur.fetchone())
+        if cur.fetchone() is None:
+            cur.execute("SELECT channel FROM channels where category = %s", (category, ))
+            for channel in cur.fetchall():
+                await client.send_message(client.get_channel(''.join(channel)), news.link)
+            cur.execute("UPDATE news SET link = %s WHERE platform = %s", (news.link, category))
+            conn.commit()
+        #except BaseException:
+            #pass
             
-        await asyncio.sleep(60)
 
 
 @client.event
